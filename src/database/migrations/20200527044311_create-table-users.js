aaf64feaa46c461982feb4bf3621a3547d75cfc6
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-05-29 14:18:26 -0300

    register e login

--- src/database/migrations/20200527044311_create-table-users.js
+++ src/database/migrations/20200527044311_create-table-users.js
@@ -4,6 +4,10 @@ exports.up = function(knex) {
     table.increments();
     table.string('name').notNullable();
     table.string('email').unique().notNullable();
+    table.string('facebook');
+    table.string('github').notNullable();
+    table.integer('code').default(null);
+    table.integer('status').default(0);
     table.string('password').notNullable();
     table.timestamp('created_at').defaultTo(knex.fn.now());
 
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { celebrate, Segments, Joi } from 'celebrate'
 const routes = express.Router()
 import AuthMiddleware from './middlewares/auth'
 import HomeController from './controllers/HomeController'
@@ -6,9 +7,23 @@ import AuthController from './controllers/AuthController'
 
 routes.get('/', HomeController.index)
 routes.get('/dashboard', AuthMiddleware, HomeController.dashboard)
-routes.post('/auth/register', AuthController.create)
-routes.get('/users', AuthMiddleware, AuthController.index)
-routes.get('/auth/sign', AuthController.authenticate)
-
+routes.post('/auth/register', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6),
+    facebook: Joi.string().allow('', null),
+    github: Joi.string().required(),
+    code: Joi.number().allow('', null)
+  })
+}), AuthController.create)
+routes.get('/users', AuthController.index)
+routes.delete('/users/:id', AuthController.excluir)
+routes.post('/auth/sign', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6)
+  })
+}), AuthController.authenticate)
 
 export default routes
