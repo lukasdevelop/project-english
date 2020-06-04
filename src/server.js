34d16da83e2f3d593e5f1f7a859384db0b7c2ae7
Lucas Amaral <lucasamaral@MacBook-Pro-de-Lucas.local>
2020-06-04 12:55:22 -0300

    websocket

--- src/server.js
+++ src/server.js
@@ -1,5 +1,26 @@
 import app from './app'
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.listen(process.env.PORT|| 3333, () => {
+io.on('connection', socket =>{
+  console.log(`socket connected: ${socket.id}`)
+  const votes = () => {
+    return 0 + Math.floor((100 - 0) * Math.random())
+  }
+  const text = {
+    title: "Project For Kids",
+    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.",
+    author: "Lukas Conka",
+ }
+ let dados = []
+  setInterval(() =>{
+    text.votes = votes()
+    dados.push(text)
+    socket.emit('FromApi', dados)
+
+  }, 7000)
+
+})
+server.listen(process.env.PORT || 3333, () => {
   console.log('Server ON at PORT', process.env.PORT)
 })
